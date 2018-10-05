54a59fc8b22885c1337168131420c79aa146cf65
Lukas Winkler <github@lw1.at>
2018-10-05 17:24:34 +0200

    rename gnupg2 to gnupg
    It seems like the package has been renamed

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -x
-apt -y install make clang openssl-dev perl tsu wget git python python-dev gnupg2 dirmngr curl autoconf automake sed gettext gzip pkg-config
+apt -y install make clang openssl-dev perl tsu wget git python python-dev gnupg dirmngr curl autoconf automake sed gettext gzip pkg-config
 
 
 pip install virtualenv
